e9b6c0082bb4f5f01cbdd2070515274960d60e46
Eduard Abdullin <eabdullin@cloudlinux.com>
2021-03-14 23:49:28 +0300

    Finish 07_Module

--- 07_Module/ex02/Array.hpp
+++ 07_Module/ex02/Array.hpp
@@ -5,40 +5,33 @@
 #include <string>
 
 template <class T>
-class Array{
+ class Array{
 	private:
 		T *_array;
 		unsigned int _size;
 
 	public:
-		Array(){
-			_size = 0;
-			_array = new T[_size];
-		};
-		Array(unsigned int size){
-			_size = size;
-			_array = new T[_size];
-		};
+		Array(): _array(new T[0]), _size(0){};
+		Array(unsigned int size): _array(new T[size]), _size(size){};
 		Array(Array const &obj){*this = obj;};
 		~Array(){delete [] _array;};
 
-		int size(void) const {return (_size);};
+		unsigned int size(void) const {return (_size);};
 
-		T &operator=(Array const &obj){
-			*this->_size = obj.size();
-			*this->_array = new T[_size];
-			for (unsigned int i = 0; i < _size; i++){
-				*this->_array[i] = obj._array[i];
-			}
+		Array &operator=(Array const &obj){
+			_size = obj.size();
+			delete [] _array;
+			_array = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_array[i] = obj._array[i];
 			return (*this);
 		};
+
 		T& operator[](unsigned int index)
 		{
 			if (_size - 1 < index)
 				throw ArrayIndexOutOfBoundsException();
-			else
-				return (_array[index]);
-			return NULL;
+			return (_array[index]);
 		}
 
 		class ArrayIndexOutOfBoundsException: public std::exception {
--- 07_Module/ex02/main.cpp
+++ 07_Module/ex02/main.cpp
@@ -1,6 +1,28 @@
+#include <iostream>
+#include <string>
+
 #include "Array.hpp"
 
 int	main(void)
 {
+	Array<int> arr1 = Array<int>();
+	Array<int> arr2 = Array<int>(5);
+	Array<double> arr3 = Array<double>(5);
+	Array<std::string> arr_str = Array<std::string>(10);
+
+	arr1 = arr2;
+	try{
+		for (unsigned int i = 0; i < arr2.size() + 1; i++)
+			arr2[i] = 10;
+	}catch(std::exception &exception){
+		std::cout << exception.what() << std::endl;
+	}
+	try{
+		for (unsigned int i = 0; i < arr2.size() + 1; i++)
+			std::cout << "i-" << i << " = " << arr2[i] << std::endl;
+	}catch(std::exception &exception){
+		std::cout << exception.what() << std::endl;
+	}
+
 	return (0);
 }
